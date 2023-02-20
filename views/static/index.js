3e13e4f5995d0f1af9e505a7d59dcb64db9a9e5b
besugos <besugos@gmail.com>
2023-02-20 17:47:57 -0300

    feat: added click handling on datatable

--- views/static/index.js
+++ views/static/index.js
@@ -4,8 +4,19 @@ $(document).ready( async function () {
     axios.get('http://localhost:3000/payments')
         .then(function (response) {
             let arrayData = mapper(response.data);
-            $('#table_id').DataTable({
+            let table = $('#table_id').DataTable({
                 data: arrayData,
+                columnDefs: [
+                    {
+                        targets: -1,
+                        data: null,
+                        defaultContent: '<button class="btn btn-secondary">Details</button>',
+                    },
+                ],
+            });
+            $('#table_id tbody').on('click', 'button', function () {
+                let data = table.row($(this).parents('tr')).data();
+                alert("You clicked on id " + data[0]);
             });
         })
         .catch(function (error) {
@@ -16,6 +27,8 @@ $(document).ready( async function () {
 $("#new-payment").click(function() {
     window.location.href = 'http://localhost:3000/cadastro';
 })
+
+
 
 
 function mapper(data) {
