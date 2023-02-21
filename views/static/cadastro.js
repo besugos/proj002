c2eb255c51afadcd8d44baecaa0483f22de0d102
besugos <besugos@gmail.com>
2023-02-21 13:33:36 -0300

    feat: implemented new payment creation

--- views/static/cadastro.js
+++ views/static/cadastro.js
@@ -11,3 +11,38 @@ $("#paymentMethod").on("change", function () {
     }
 })
 
+function mountBody() {
+    let body = {
+        name: $('#buyerName').val(),
+        email: $('#buyerEmail').val(),
+        cpf: $('#buyerCpf').val(),
+        amount: $('#paymentAmount').val(),
+        type: $('#paymentMethod').val(),
+    }
+
+    if (body.type === 'card') {
+        body.holder = $('#cardHolder').val();
+        body.number = $('#cardNumber').val();
+        body.expiration = $('#cardExp').val();
+        body.cvc = $('#cardCvc').val();
+    }
+    console.log(body);
+    return body;
+}
+
+async function postNewPayment(paymentBody) {
+    axios.post(`http://localhost:3000/create`, paymentBody)
+        .then(function (response) {
+            console.log('Criado com sucesso');
+            return;
+        })
+        .catch(function (error) {
+            console.log(error);
+        })
+}
+
+$("#savePayment").click(async function() {
+    let paymentBody = mountBody();
+    await postNewPayment(paymentBody);
+    window.location.href = 'http://localhost:3000';
+})
